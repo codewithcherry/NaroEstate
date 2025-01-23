48a1460d4e540a195c832a00103e1e349f9dacb0
Prashanth Naroju <prashanth.naro@gmail.com>
2025-01-23 14:50:22 +0530

    complete register page ui with registerForm component

--- naro-estate/src/app/(authenctication)/register/page.jsx
+++ naro-estate/src/app/(authenctication)/register/page.jsx
@@ -1,9 +1,12 @@
+import RegisterForm from '@/components/react-components/RegisterForm'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>
-      Register page
+    <div className="flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10">
+      <div className="w-full max-w-sm md:max-w-3xl">
+        <RegisterForm />
+      </div>
     </div>
   )
 }
