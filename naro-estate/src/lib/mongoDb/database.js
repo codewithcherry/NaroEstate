5d683848986d5075a848cc5553f8fdc2421c18a8
Prashanth Naroju <prashanth.naro@gmail.com>
2025-01-24 15:39:40 +0530

    install mongodb package create mongo db connection function

--- naro-estate/src/lib/mongoDb/database.js
+++ naro-estate/src/lib/mongoDb/database.js
@@ -1,1 +1,25 @@
-// This is mongo db connection and config file
+// MongoDB connection and config file
+import mongoose from "mongoose";
+
+const MONGO_URI = process.env.MONGO_URI;
+
+const connect = async () => {
+  try {
+    // Attempt to connect to MongoDB
+    const connection = await mongoose.connect(MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    // Connection successful
+    console.log(`MongoDB connected: ${connection.connection.host}`);
+  } catch (error) {
+    // Log the error message
+    console.error(`Error connecting to MongoDB: ${error.message}`);
+    
+    // Exit the process with a failure code if connection fails
+    process.exit(1);
+  }
+};
+
+export default connect;
