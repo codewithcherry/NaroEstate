5b612d57027bb10af7134af629f7c4c839c3a1f6
Prashanth Naroju <prashanth.naro@gmail.com>
2025-01-24 15:57:56 +0530

    create user model and schema for the same to create new user

---
+++ naro-estate/src/lib/models/user.model.js
@@ -0,0 +1,53 @@
+import mongoose from "mongoose";
+import { Schema } from "mongoose";
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  firstname: {
+    type: String,
+  },
+  lastname: {
+    type: String,
+  },
+  role: {
+    type: String,
+    enum: ["user", "admin", "super-admin", "moderator"],
+    default: "user",
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  imageUrl: {
+    type: String,
+  },
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+  verifyToken: {
+    type: String,
+  },
+  verifyTokenExpiry: {
+    type: Date,
+  },
+  resetToken: {
+    type: String,
+  },
+  resetTokenExpiry: {
+    type: Date,
+  },
+}, { timestamps: true });
+
+// Check if the model is already defined in Mongoose's internal registry
+const User = mongoose.models.User || mongoose.model("User", userSchema);
+
+export default User;
