973668d199d24809223a5997ee2ba4f4d25b5b45
Prashanth Naroju <prashanth.naro@gmail.com>
2025-01-28 13:54:21 +0530

    fix 2.0 of verify email page

--- naro-estate/src/app/(authenctication)/verify-email/page.js
+++ naro-estate/src/app/(authenctication)/verify-email/page.js
@@ -7,14 +7,14 @@ import Link from 'next/link';
 const Page = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [message, setMessage] = useState('');
-  const [queryParams, setQueryParams] = useState({ token: null, userId: null });
+  const [queryParams, setQueryParams] = useState({ verifyToken: null, userId: null });
 
   useEffect(() => {
     // Extract query parameters from URL
     const searchParams = new URLSearchParams(window.location.search);
-    const token = searchParams.get('token');
+    const VerifyToken = searchParams.get('token');
     const userId = searchParams.get('userId');
-    setQueryParams({ token, userId });
+    setQueryParams({ VerifyToken, userId });
   }, []);
 
   const handleVerify = async () => {
