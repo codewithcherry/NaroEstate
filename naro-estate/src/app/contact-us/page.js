9fa791b1d1c2d3a17b9e0ce76b9c102e01229124
Prashanth Naroju <prashanth.naro@gmail.com>
2025-02-03 11:46:40 +0530

    create page.js files for all navlinks to route

---
+++ naro-estate/src/app/contact-us/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      This contact-us page
+    </div>
+  )
+}
+
+export default page
---
+++ naro-estate/src/app/listings/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      This is listing page
+    </div>
+  )
+}
+
+export default page
