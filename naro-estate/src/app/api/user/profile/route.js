f43eae1ae38d82f03365b8f1b17052527a1ea3aa
Prashanth Naroju <prashanth.naro@gmail.com>
2025-02-05 15:40:15 +0530

    code the profile page route for fetching get for user profile info

---
+++ naro-estate/src/app/api/user/profile/route.js
@@ -0,0 +1,62 @@
+import { NextResponse } from "next/server";
+import User from "@/lib/models/user.model";
+
+export const GET = async (request) => {
+    try {
+        // Access headers from the request
+        const userId = request.headers.get("userId");
+        console.log(userId);
+
+        // Validate userId
+        if (!userId) {
+            return NextResponse.json(
+                {
+                    type: "error",
+                    message: "User ID is required in the headers.",
+                },
+                {
+                    status: 400,
+                }
+            );
+        }
+
+        // Fetch user from the database
+        const user = await User.findById(userId).select("-password"); // Exclude password field
+
+        if (!user) {
+            return NextResponse.json(
+                {
+                    type: "error",
+                    message: "User not found. Try again later.",
+                },
+                {
+                    status: 404,
+                }
+            );
+        }
+
+        // Return user data excluding password
+        return NextResponse.json(
+            {
+                type: "success",
+                message: "User retrieved successfully.",
+                user,
+            },
+            {
+                status: 200,
+            }
+        );
+
+    } catch (error) {
+        console.error("Error fetching user:", error);
+        return NextResponse.json(
+            {
+                type: "error",
+                message: "Something went wrong! Internal Server Error.",
+            },
+            {
+                status: 500,
+            }
+        );
+    }
+};
--- naro-estate/src/app/profile/page.js
+++ naro-estate/src/app/profile/page.js
@@ -1,6 +1,42 @@
-import React from 'react'
+'use client';
+
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const page = () => {
+
+  const [profileData,setProfileData]=useState({});
+  const [loading,setLoading]=useState(false);
+
+  const token=localStorage.getItem('authToken');
+
+  const getProfileData=async () => {
+    try {
+
+      setLoading(true);
+
+      const response= await axios.get('/api/user/profile',{
+        headers:{
+          'Authorization': `Bearer ${token}`, 
+          'Content-Type':'application/json'
+        }
+      })
+
+      console.log(response.data);
+      setProfileData(response.data?.user);
+    } catch (error) {
+      console.log(error);
+    }
+    finally{
+      setLoading(false);
+    }
+  }
+
+  useEffect(()=>{
+    console.log(token)
+    getProfileData();
+  },[])
+
   return (
     <div>
       This is profile page
