5cf52192d04643d4d0dd497eb11cc0bdd70c1aa0
Prashanth Naroju <prashanth.naro@gmail.com>
2025-02-25 13:59:32 +0530

    add the New Listing button to navigate to create-listing path

--- naro-estate/src/app/my-listings/page.js
+++ naro-estate/src/app/my-listings/page.js
@@ -1,9 +1,20 @@
+'use client';
+import { Button } from '@/components/ui/button'
+import { Plus } from 'lucide-react'
+import { useRouter } from 'next/navigation'
 import React from 'react'
 
 const page = () => {
+
+  const router =useRouter();
+
+  const handleCreateNewListing=()=>{
+      router.push('/create-listing');
+  }
   return (
-    <div>
-      This is the user listings page
+    <div className='flex flex-end'>
+      
+      <Button onClick={handleCreateNewListing}><Plus />New Listing</Button>
     </div>
   )
 }
